a52a79d99b79f615a22965411e62142a9b9e8e67
irshalfir <irshalfir@gmail.com>
2021-02-15 16:01:01 +0700

    add bulk coroutine test

--- kotlin-coroutines/app/src/test/kotlin/com/playground/kotlin/coroutines/CoroutineTest.kt
+++ kotlin-coroutines/app/src/test/kotlin/com/playground/kotlin/coroutines/CoroutineTest.kt
@@ -5,10 +5,12 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
+import java.util.*
+import kotlin.concurrent.thread
 
 class CoroutineTest {
 
-    suspend fun hello() {
+    private suspend fun hello() {
         delay(1_000)
         println("Hello World")
     }
@@ -20,6 +22,34 @@ class CoroutineTest {
         println("WAITING...")
         runBlocking {
             delay(2_000)
+        }
+        println("FINISH")
+    }
+
+    @Test
+    fun testThread() {
+        repeat(1000) {
+            thread {
+                Thread.sleep(1_000)
+                println("Done $it : ${Date()}")
+            }
+        }
+        println("WAITING...")
+        Thread.sleep(10_000)
+        println("FINISH")
+    }
+
+    @Test
+    fun testBulkCoroutine() {
+        repeat(100_000) {
+            GlobalScope.launch {
+                delay(1_000)
+                println("Done $it : ${Date()} : ${Thread.currentThread().name}")
+            }
+        }
+        println("WAITING...")
+        runBlocking {
+            delay(3_000)
         }
         println("FINISH")
     }
