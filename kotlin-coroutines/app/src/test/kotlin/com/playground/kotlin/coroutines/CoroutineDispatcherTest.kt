f8eb5ad60c28f0acb38f561a763f4a0f0843b23d
irshalfir <irshalfir@gmail.com>
2021-02-15 20:30:21 +0700

    extending coroutine dispatcher

---
+++ kotlin-coroutines/app/src/test/kotlin/com/playground/kotlin/coroutines/CoroutineDispatcherTest.kt
@@ -0,0 +1,113 @@
+package com.playground.kotlin.coroutines
+
+import kotlinx.coroutines.*
+import org.junit.jupiter.api.Test
+import java.util.concurrent.Executors
+
+class CoroutineDispatcherTest {
+
+    @Test
+    fun testDispatcher() {
+        runBlocking {
+            println("runBlocking ${Thread.currentThread().name}")
+            val job1 = GlobalScope.launch(Dispatchers.Default) {
+                println("Job 1 ${Thread.currentThread().name}")
+            }
+            val job2 = GlobalScope.launch(Dispatchers.IO) {
+                println("Job 2 ${Thread.currentThread().name}")
+            }
+            joinAll(job1, job2)
+        }
+    }
+
+    @Test
+    fun testUnconfined() {
+        runBlocking {
+            println("runBlocking : ${Thread.currentThread().name}")
+            GlobalScope.launch(Dispatchers.Unconfined) {
+                println("Unconfined: ${Thread.currentThread().name}")
+                delay(1_000)
+                println("Unconfined: ${Thread.currentThread().name}")
+            }
+            // Confined
+            GlobalScope.launch {
+                println("Unconfined: ${Thread.currentThread().name}")
+                delay(1_000)
+                println("Unconfined: ${Thread.currentThread().name}")
+            }
+            delay(2000)
+        }
+    }
+
+    @Test
+    fun testExecutorService() {
+        val dispatcherService = Executors.newFixedThreadPool(10).asCoroutineDispatcher()
+        val dispatcherWeb = Executors.newSingleThreadExecutor().asCoroutineDispatcher()
+
+        runBlocking {
+            val job1 = GlobalScope.launch(dispatcherService) {
+                println("Job 1 : ${Thread.currentThread().name}")
+            }
+            val job2 = GlobalScope.launch(dispatcherWeb) {
+                println("Job 2 : ${Thread.currentThread().name}")
+            }
+            joinAll(job1, job2)
+        }
+    }
+
+    @Test
+    fun testWithContext() {
+        val dispatcherClient = Executors.newFixedThreadPool(10).asCoroutineDispatcher()
+        runBlocking {
+            val job = GlobalScope.launch(Dispatchers.IO) {
+                println("1 ${Thread.currentThread().name}")
+                withContext(dispatcherClient) {
+                    println("2 ${Thread.currentThread().name}")
+                }
+                println("3 ${Thread.currentThread().name}")
+            }
+            job.join()
+        }
+    }
+
+    @Test
+    fun testCancelFinally() {
+        runBlocking {
+            val job = GlobalScope.launch {
+                try {
+                    println("Start Job")
+                    delay(1000)
+                    println("End Job")
+                } finally {
+                    println(isActive)
+                    delay(1000)
+                    println("Finally")
+                }
+            }
+            job.cancelAndJoin()
+        }
+    }
+
+
+    @Test
+    fun testNonCancellable() {
+        runBlocking {
+            val job = GlobalScope.launch {
+                try {
+                    println("Start Job")
+                    delay(1000)
+                    println("End Job")
+                } finally {
+                    println(isActive)
+                    withContext(NonCancellable) {
+                        println(isActive)
+                        delay(1000)
+                        println("Finally")
+                    }
+                }
+            }
+            job.cancelAndJoin()
+        }
+    }
+
+}
