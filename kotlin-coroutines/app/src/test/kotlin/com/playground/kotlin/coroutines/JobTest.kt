fb9bcc9c00477b761a916ed85cd11ccd796362f5
irshalfir <irshalfir@gmail.com>
2021-02-15 16:10:59 +0700

    add Job

---
+++ kotlin-coroutines/app/src/test/kotlin/com/playground/kotlin/coroutines/JobTest.kt
@@ -0,0 +1,53 @@
+package com.playground.kotlin.coroutines
+
+import kotlinx.coroutines.*
+import org.junit.jupiter.api.Test
+
+class JobTest {
+
+    @Test
+    fun testJob() {
+        runBlocking {
+            GlobalScope.launch {
+                delay(2_000)
+                println("Coroutine Done : ${Thread.currentThread().name}")
+            }
+        }
+    }
+
+    @Test
+    fun testLazyJob() {
+        runBlocking {
+            val job: Job = GlobalScope.launch(start = CoroutineStart.LAZY) {
+                delay(2_000)
+                println("Coroutine Done : ${Thread.currentThread().name}")
+            }
+            job.start()
+            delay(3_000)
+        }
+    }
+
+    @Test
+    fun testJoinJob() {
+        runBlocking {
+            val job: Job = GlobalScope.launch() {
+                delay(2_000)
+                println("Coroutine Done : ${Thread.currentThread().name}")
+            }
+            job.join()
+        }
+    }
+
+    @Test
+    fun testCancelJob() {
+        runBlocking {
+            val job: Job = GlobalScope.launch() {
+                delay(2_000)
+                println("Coroutine Done : ${Thread.currentThread().name}")
+            }
+            job.cancel()
+            delay(3_000)
+        }
+    }
+
+}
