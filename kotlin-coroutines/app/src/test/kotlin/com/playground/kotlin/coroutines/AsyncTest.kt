2938abb674165f34d9f775353085405be2a2f064
irshalfir <irshalfir@gmail.com>
2021-02-15 17:12:12 +0700

    add async function and sequential suspend function

---
+++ kotlin-coroutines/app/src/test/kotlin/com/playground/kotlin/coroutines/AsyncTest.kt
@@ -0,0 +1,33 @@
+package com.playground.kotlin.coroutines
+
+import kotlinx.coroutines.*
+import org.junit.jupiter.api.Test
+import kotlin.system.measureTimeMillis
+
+class AsyncTest {
+
+    suspend fun getFoo(): Int {
+        delay(1000)
+        return 10
+    }
+
+    suspend fun getBar(): Int {
+        delay(1000)
+        return 10
+    }
+
+    @Test
+    fun testAsync() {
+        runBlocking {
+            val time = measureTimeMillis {
+                val foo = GlobalScope.async { getFoo() }
+                val bar: Deferred<Int> = GlobalScope.async { getBar() }
+
+                val result = foo.await() + bar.await()
+                println("Result: $result")
+            }
+            println("Total time: $time")
+        }
+    }
+
+}
